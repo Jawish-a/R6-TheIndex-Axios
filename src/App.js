cc90bc32d480ba4e0787fd5a018dba00b95dcd0f
jawish-a@outlook.com <jawish-a@outlook.com>
2020-10-05 20:11:33 +0300

    task done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-import authors from "./data.js";
+// import authors from "./data.js";
+import axios from "axios";
 // Components
 import Sidebar from "./Sidebar";
 import AuthorList from "./AuthorList";
@@ -8,8 +9,46 @@ import AuthorDetail from "./AuthorDetail";
 
 const App = () => {
   const [currentAuthor, setCurrentAuthor] = useState(null);
+  const [authors, setAuthors] = useState([])
+  const [loading, setLoading] = useState(true)
 
-  const selectAuthor = author => setCurrentAuthor(author);
+  useEffect( () => {
+    axios.get("https://the-index-api.herokuapp.com/api/authors/")
+    .then((response) => {
+      // console.log(response.data)
+      setAuthors(response.data)
+      setLoading(false)
+    })
+    .catch((error) => {
+      // handle error
+      console.log(error);
+    })
+  }, [])
+
+  // useEffect( () => {
+  //   axios.get(`https://the-index-api.herokuapp.com/api/authors/${author.id}`)
+  //   .then((response) => {
+  //     console.log(response.data);
+  //     setCurrentAuthor(author);
+  //     setLoading(false);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   })
+  // } );
+  const selectAuthor = author => {
+    
+      axios.get(`https://the-index-api.herokuapp.com/api/authors/${author.id}`)
+      .then((response) => {
+        console.log(response.data);
+        setCurrentAuthor(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+    
+  };
 
   const unselectAuthor = () => setCurrentAuthor(null);
 
@@ -27,7 +66,10 @@ const App = () => {
         <div className="col-2">
           <Sidebar unselectAuthor={unselectAuthor} />
         </div>
-        <div className="content col-10">{getContentView()}</div>
+        { loading ? 
+        <div className="content col-10"><h1>Please wait i am loading</h1></div>
+        : <div className="content col-10">{getContentView()}</div> }
+        
       </div>
     </div>
   );
